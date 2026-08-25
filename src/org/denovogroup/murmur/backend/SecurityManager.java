43c1ead59aee628ef363707fa7ce321592c3c5b8
LightTerranova <dudiy@ualberta.ca>
2026-08-25 16:05:15 -0600

    Change to max number of messages

--- src/org/denovogroup/murmur/backend/SecurityManager.java
+++ src/org/denovogroup/murmur/backend/SecurityManager.java
@@ -102,7 +102,7 @@ public class SecurityManager {
                 .setAutodeleteAge(14)
                 .setShareLocation(true)
                 .setMinSharedContacts(0)
-                .setMaxMessages(1200) // for Moby testing
+                .setMaxMessages(10000) // for Moby testing
                 .setCooldown(5)
                 .setEnforceLock(false)
                 .setUseTrust(false)
